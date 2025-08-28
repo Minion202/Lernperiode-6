86d3a97762911762f89ef1b0dc273d2267a3a565
Mina Urgancio <M.Urgancio.inf24@stud.bbbaden.ch>
2025-08-28 20:52:53 +0200

    Responsive

--- Lego Ninjago /MainWindow.axaml.cs
+++ Lego Ninjago /MainWindow.axaml.cs
@@ -1,20 +1,23 @@
 using Avalonia.Controls;
+using System.Collections.Generic;
 
-namespace Lego_Ninjago;
-
-
-public partial class MainWindow : Window
+namespace Lego_Ninjago
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        
-                    Ninja cole = new Ninja("Cole");
-                    Ninja kai = new Ninja("Kai");
-                    Ninja jay = new Ninja("Jay");
-                    Ninja zane = new Ninja("Zane");
-                    Ninja lloyd = new Ninja("Lloyd");
-   
+        private readonly Dictionary<string, Ninja> _ninjas;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _ninjas = NinjaService.BuildAll();
+
+            ToolTip.SetTip(BtnCole,  _ninjas["COLE"].BuildTooltip());
+            ToolTip.SetTip(BtnJay,   _ninjas["JAY"].BuildTooltip());
+            ToolTip.SetTip(BtnLloyd, _ninjas["LLOYD"].BuildTooltip());
+            ToolTip.SetTip(BtnKai,   _ninjas["KAI"].BuildTooltip());
+            ToolTip.SetTip(BtnZane,  _ninjas["ZANE"].BuildTooltip());
+        }
     }
 }
-
--- Lego Ninjago /Ninja.cs
+++ Lego Ninjago /Ninja.cs
@@ -2,12 +2,22 @@ namespace Lego_Ninjago
 {
     public class Ninja
     {
-        public Ninja(string name)
+        public Ninja(string name, string element, string[] abilities, string[] strengths)
         {
-            this.Name = name;
+            Name = name;
+            Element = element;
+            Abilities = abilities ?? [];
+            Strengths = strengths ?? [];
         }
 
-        public string Name { get; set; }
+        public string Name { get; }
+        public string Element { get; }
+        public string[] Abilities { get; }
+        public string[] Strengths { get; }
+
+        public string BuildTooltip() =>
+            $"{Name} – Element: {Element}\n" +
+            $"Abilities: {string.Join(", ", Abilities)}\n" +
+            $"Stärken: {string.Join(", ", Strengths)}";
     }
 }
-
