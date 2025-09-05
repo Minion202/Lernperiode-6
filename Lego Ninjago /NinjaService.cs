59fd87568e426c713e30aac490b534fe6b3a5cdc
Mina Urgancio <M.Urgancio.inf24@stud.bbbaden.ch>
2025-09-05 12:32:29 +0200

    slideshow

---
+++ Lego Ninjago /NinjaService.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Lego_Ninjago
+{
+    public static class NinjaService
+    {
+        public static Dictionary<string, Ninja> BuildAll()
+        {
+            return new Dictionary<string, Ninja>
+            {
+                ["COLE"]  = new Ninja("Cole",  "Erde",    new[] {"Erdbeben-Punch", "Bodenwelle"},           new[] {"Tank", "Standfestigkeit"}),
+                ["JAY"]   = new Ninja("Jay",   "Blitz",   new[] {"Kettenblitz", "Schneller Dash"},          new[] {"Tempo", "Reichweite"}),
+                ["LLOYD"] = new Ninja("Lloyd", "Energie", new[] {"Energie-Schwert", "Auren-Schild"},        new[] {"Allrounder", "Führung"}),
+                ["KAI"]   = new Ninja("Kai",   "Feuer",   new[] {"Flammen-Wirbel", "Feuerstoß"},            new[] {"Burst-Damage", "Aggressiv"}),
+                ["ZANE"]  = new Ninja("Zane",  "Eis",     new[] {"Frost-Pfeil", "Eisige Gefangenschaft"},   new[] {"Kontrolle", "Präzision"})
+            };
+        }
+    }
+}
